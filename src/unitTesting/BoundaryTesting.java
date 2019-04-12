bb2b69d531b9e53485dc9392b10001dfb61ab6a7
Elisha Arulchelvan <elisha.a@live.com>
2019-04-12 17:46:12 +0100

    BoundaryTest classes changed

--- src/unitTesting/BoundaryTesting.java
+++ src/unitTesting/BoundaryTesting.java
@@ -4,58 +4,24 @@ import java.util.Scanner;
 
 public class BoundaryTesting {
 	
-	/*private static int num;
 	
-	
-	
-	public int getNum() {
-		return num;
-	}
-
-
-
-	public void setNum(int num) {
-		this.num = num;
-	}
-
-
-
-	public static void main(String args[])
-	{
-		
-		System.out.println("Max pizzas you can order is 10");
-		System.out.println("How many pizzas do you want?");
-		
-		//Scanner inp= new Scanner(System.in);
-		//int num;
-		//num = inp.nextInt();
-		if(num <=10)
-		{
-			System.out.println("Youve ordered " + num + " pizzas");
-		}
-		else
-		{
-			System.out.println("You can order that number of pizzas");
-		}
-	} */
-	
-	public void makePizza(int number) throws IllegalArgumentException
+	public void makePizza(int[] pizzas) throws IllegalArgumentException
 	{
 		System.out.println("Max pizzas you can order is 10");
 		System.out.println("How many pizzas do you want?");
 		
-		if(number <=10 )
+		int inputPizza = 0;
+		
+		int max = 10;
+		int min = 1;
+		
+		if(pizzas.length <= max || pizzas.length >= min)
 		{
-			System.out.println("Youve ordered " + number + " pizzas");
+			System.out.println(pizzas.length + "pizzas ordered");
 		}
-		else if(number >=1)
-		{
-			System.out.println("Youve ordered " + number + " pizzas");
-		}
-		else
-		{
-			System.out.println("You can order that number of pizzas");
-		}
+		System.out.println(inputPizza + "not right amount of pizzas ordered");
+		
+		
 	}
 
 }
--- src/unitTesting/Test/BoundaryTestingTest.java
+++ src/unitTesting/Test/BoundaryTestingTest.java
@@ -1,6 +1,7 @@
 package unitTesting.Test;
 
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -8,12 +9,22 @@ import unitTesting.BoundaryTesting;
 
 public class BoundaryTestingTest {
 
+	int[] maxBoundary = new int[10];
+	int[] minBoundary = new int[1];
+	
 	@Test
-	public void testMain() {
+	public void testMax() {
 		
 		BoundaryTesting bTest = new BoundaryTesting();
-		bTest.makePizza(-20);
+		assertEquals(10, bTest.makePizza(maxBoundary));
 		
+	}
+	
+	@Test
+	public void testMin()
+	{
+		BoundaryTesting bTest = new BoundaryTesting();
+		assertEquals(1, bTest.makePizza(minBoundary));
 	}
 
 }
